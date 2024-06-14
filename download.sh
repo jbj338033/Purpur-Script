62612728fa12dd4c1cbee1cccaa3c6a6c21541fa
전민오 <jbj338033@gmail.com>
2024-06-14 16:16:52 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,6 +2,6 @@ VERSION=$(curl -s https://api.purpurmc.org/v2/purpur | \
     jq -r '.versions[-1]')
 BUILD=$(curl -s https://api.purpurmc.org/v2/purpur/${VERSION} | \
     jq -r '.builds | .latest')
-URL="https://api.purpurmc.org/v2/purpur/${VERSION}/${BUILD}/downloads/"
+URL="https://api.purpurmc.org/v2/purpur/${VERSION}/${BUILD}/download"
 
 curl -o purpur.jar $URL
